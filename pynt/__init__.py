6f9f931f568bd15c12a17f417feb23a26ce29737
LI Yu <liyu1981@gmail.com>
2015-04-30 00:34:58 +0800

    Update __init__.py
    Expose build function

--- pynt/__init__.py
+++ pynt/__init__.py
@@ -5,9 +5,9 @@ Lightweight Python Build Tool
 __version__ = "0.8.1"
 __license__ = "MIT License"
 __contact__ = "http://rags.github.com/pynt/"
-from ._pynt import task, main
+from ._pynt import task, main, build
 import pkgutil
 
 __path__ = pkgutil.extend_path(__path__,__name__)
 
-__all__ = ["task",  "main"]
+__all__ = ["task",  "main", "build"]
